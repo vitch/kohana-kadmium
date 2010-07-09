70456a2db322230dff2e2dffd7e80c2da0ae92ee
Kelvin Luck <git@kelvinluck.com>
2010-07-09 20:33:48 +0200

    Added support for the HasOne field display() method so that it displays correctly in list pages...

---
+++ classes/kadmium/field/hasone.php
@@ -0,0 +1,10 @@
+<?php defined('SYSPATH') or die('No direct script access.');
+
+abstract class Kadmium_Field_HasOne extends Jelly_Field_HasOne
+{
+
+	public function display($model, $value)
+	{
+		return $value->execute()->{Jelly::meta($this->foreign['model'])->name_key()};
+	}
+}
