01679a65b8637341fc97cd8a1e3a41f9e38353a0
Kelvin Luck <git@kelvinluck.com>
2010-07-09 19:16:23 +0200

    Adding in overrides of basic Jelly functionality which is useful for Kadmium... If you want to override Jelly_Field make sure you extend Kadmium_Field instead of Jelly_Field_Core...

---
+++ classes/jelly/field.php
@@ -0,0 +1,5 @@
+<?php defined('SYSPATH') or die('No direct script access.');
+
+abstract class Jelly_Field extends Kadmium_Field
+{
+}
---
+++ classes/kadmium/field.php
@@ -0,0 +1,6 @@
+<?php defined('SYSPATH') or die('No direct script access.');
+
+abstract class Kadmium_Field extends Kadmium_Field_Core
+{
+
+}
