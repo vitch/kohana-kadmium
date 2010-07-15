a36e60b77428b7ca5c0a241fb31c5648204c5390
Kelvin Luck <git@kelvinluck.com>
2010-07-15 16:02:57 +0100

    Added an override for Jelly_Field_ManyToMany to allow the jquery-asmselect to display a nice title in the select box...

---
+++ classes/kadmium/field/manytomany.php
@@ -0,0 +1,19 @@
+<?php defined('SYSPATH') or die('No direct script access.');
+
+class Kadmium_Field_ManyToMany extends Jelly_Field_ManyToMany
+{
+	/**
+	 * Adds a title to the attributes array to work nicely with jquery-asmselect
+	 *
+	 * @param string $prefix
+	 * @param array $data
+	 * @return View
+	 */
+	public function input($prefix = 'jelly/field', $data = array())
+	{
+		$attrs = Arr::get($data, 'attributes', array());
+		$attrs['title'] = 'Select ' . $this->label . '...';
+		$data['attributes'] = $attrs;
+		return parent::input($prefix, $data);
+	}
+}
