77af588627e9932229c24bbe006297d7295cc622
Kelvin Luck <git@kelvinluck.com>
2010-07-16 20:55:58 +0100

    Added the ability for a HasManyUniquely field to supress the lightbox method of display...

--- views/jelly/field/hasmanyuniquely.php
+++ views/jelly/field/hasmanyuniquely.php
@@ -1,10 +1,11 @@
 <ul class="has-many-uniquely" id="<?= $field->name; ?>">
 	<?php
 	foreach($value as $child_model):
+		$lb_class = isset($field->prevent_lightbox) && $field->prevent_lightbox ? '' : ' lb';
 	?>
-		<li><a href="<?= sprintf($field->edit_link_base, $model->id()) . $child_model->id(); ?>" class="edit lb"><?= $child_model->name(); ?></a></li>
+		<li><a href="<?= sprintf($field->edit_link_base, $model->id()) . $child_model->id(); ?>" class="edit<?= $lb_class; ?>"><?= $child_model->name(); ?></a></li>
 	<?php
 	endforeach;
 	?>
-		<li><a href="<?= sprintf($field->edit_link_base, $model->id()); ?>0" class="add lb">Add a new <?= Inflector::humanize(Jelly::model_name($value->current())); ?></a></li>
+		<li><a href="<?= sprintf($field->edit_link_base, $model->id()); ?>0" class="add<?= $lb_class; ?>">Add a new <?= Inflector::humanize(Jelly::model_name($value->current())); ?></a></li>
 </ul>
