7d961ca63fc042d8f1370762b9be07fef48200f0
Kelvin Luck <git@kelvinluck.com>
2010-07-19 12:24:42 +0100

    Enable prevention of lightbox for HasManyUniquely and optional outputting of thumbnails for listings page...

--- views/jelly/field/hasmanyuniquely.php
+++ views/jelly/field/hasmanyuniquely.php
@@ -1,13 +1,25 @@
+<?php
+	$lb_class = isset($field->prevent_lightbox) && $field->prevent_lightbox ? '' : ' lb';
+	$ul_class = isset($field->list_as_thumbnails) && $field->list_as_thumbnails ? '' : ' img-list';
+?>
 <ul class="has-many-uniquely" id="<?= $field->name; ?>">
 	<?php
 	foreach($value as $child_model):
-		$lb_class = isset($field->prevent_lightbox) && $field->prevent_lightbox ? '' : ' lb';
 	?>
 		<li>
 			<?php
+				if ($ul_class == '') {
+					$image_field = $child_model->meta()->fields($field->list_as_thumbnails);
+					$path = count($image_field->thumbnails) ? $image_field->thumbnails[0]['path'] : $image_field->path;
+					$link_contents = Html::image(
+						str_replace(DOCROOT, '', $path) . $child_model->get($field->list_as_thumbnails)
+					);
+				} else {
+					$link_contents = $child_model->name();
+				}
 				echo Html::anchor(
 					sprintf($field->edit_link_base, $model->id()) . $child_model->id(),
-					$child_model->name(),
+					$link_contents,
 					array(
 						'class' => 'edit' . $lb_class
 					)
