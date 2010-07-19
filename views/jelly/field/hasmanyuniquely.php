0f26564d85bf858e13386cc61c1a46d7dce8661f
Kelvin Luck <git@kelvinluck.com>
2010-07-19 14:03:59 +0100

    Display of optional thumbnails in child listing view working better and styled more nicely...

--- views/jelly/field/hasmanyuniquely.php
+++ views/jelly/field/hasmanyuniquely.php
@@ -1,14 +1,14 @@
 <?php
 	$lb_class = isset($field->prevent_lightbox) && $field->prevent_lightbox ? '' : ' lb';
-	$ul_class = isset($field->list_as_thumbnails) && $field->list_as_thumbnails ? '' : ' img-list';
+	$ul_class = isset($field->list_as_thumbnails) && $field->list_as_thumbnails ? ' img-list' : '';
 ?>
-<ul class="has-many-uniquely" id="<?= $field->name; ?>">
+<ul class="has-many-uniquely<?= $ul_class; ?>" id="<?= $field->name; ?>">
 	<?php
 	foreach($value as $child_model):
 	?>
 		<li>
 			<?php
-				if ($ul_class == '') {
+				if ($ul_class != '') {
 					$image_field = $child_model->meta()->fields($field->list_as_thumbnails);
 					$path = count($image_field->thumbnails) ? $image_field->thumbnails[0]['path'] : $image_field->path;
 					$link_contents = Html::image(
