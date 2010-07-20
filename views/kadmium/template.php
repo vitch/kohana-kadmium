22f19920aabe48c92b58b6d08630fa44b0565ca4
Kelvin Luck <git@kelvinluck.com>
2010-07-20 17:27:14 +0100

    Added a warning for IE browsers warning them of the less optimal experience they will have...

--- views/kadmium/template.php
+++ views/kadmium/template.php
@@ -12,6 +12,17 @@
 	</head>
 	<body>
 		<div id="kadmium-content">
+			<!--[if IE]>
+				<div id="ie-warning">
+					<p>
+						You appear to be using the Internet Explorer web browser. This system will work with Internet Explorer
+						but it is optimised for a more modern browser like <a href="http://getfirefox.com">Firefox</a>,
+						<a href="http://www.google.com/chrome">Chrome</a>, <a href="http://www.opera.com/">Opera</a> or
+						<a href="http://www.apple.com/safari/">Safari</a> - please consider downloading and installing
+						one of these alternatives to enjoy this site at its best.
+					</p>
+				</div>
+			<![endif]-->			
 			<ul class="navigation">
 			<?php
 				foreach ($navigation_controllers as $controller=>$label)
