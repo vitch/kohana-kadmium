e277e0d76dd8df58f3b18d31f2c6e07316183440
Kelvin Luck <git@kelvinluck.com>
2010-07-21 18:13:16 +0100

    Hooking a Kadmium_Model into the heirarchy including constants allowing you to specify a per-model delete_policy (e.g. in which circumstances to allow a delete)

---
+++ classes/jelly/model.php
@@ -0,0 +1,5 @@
+<?php defined('SYSPATH') or die('No direct script access.');
+
+abstract class Jelly_Model extends Kadmium_Model
+{
+}
---
+++ classes/kadmium/model/core.php
@@ -0,0 +1,14 @@
+<?php defined('SYSPATH') or die('No direct script access.');
+
+abstract class Kadmium_Model_Core extends Jelly_Model_Core
+{
+
+	// Never provide UI to delete this model
+	const DELETE_NEVER = 0;
+	// Automatically delete any children (through relationships) that this model has
+	const DELETE_ALL_CHILDREN = 1;
+	// Only allow delete to succed if there are no unique child
+	const DELETE_ONLY_SPINSTER = 2;
+
+	public $delete_policy = Kadmium_Model_Core::DELETE_ONLY_SPINSTER;
+}
