9152a0bae47cb963c8dc7b0109f19b75cd357179
Kelvin Luck <git@kelvinluck.com>
2010-11-24 15:24:54 +0000

    Using the name method when getting a belongsto field to display...

--- classes/kadmium/field/belongsto.php
+++ classes/kadmium/field/belongsto.php
@@ -5,6 +5,6 @@ abstract class Kadmium_Field_BelongsTo extends Jelly_Field_BelongsTo
 
 	public function display($model, $value)
 	{
-		return $value->execute()->{Jelly::meta($this->foreign['model'])->name_key()};
+		return $value->execute()->name();
 	}
 }
