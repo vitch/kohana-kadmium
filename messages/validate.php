7bf4804916c55cef294c1f398cd70a0c9883594e
Kelvin Luck <git@kelvinluck.com>
2010-11-26 10:43:42 +0000

    Added a message for when unique validation fails...

--- messages/validate.php
+++ messages/validate.php
@@ -1,5 +1,6 @@
 <?php defined('SYSPATH') or die('No direct script access.');
 
 return array(
-	'url'	    => '":field" must be a valid URL (including http:// at the start)',
+	'url'	    	=> '":field" must be a valid URL (including http:// at the start)',
+	'unique'		=> '":field" must be unique - that address is already registered',
 );
