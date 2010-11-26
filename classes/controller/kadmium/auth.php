747c6c8657db4f1d3357838554b1da2eb9df7b99
Kelvin Luck <git@kelvinluck.com>
2010-11-26 12:44:45 +0000

    Adding the ability to protect controllers or methods on a per-role basis...

--- classes/controller/kadmium/auth.php
+++ classes/controller/kadmium/auth.php
@@ -4,6 +4,7 @@ class Controller_Kadmium_Auth extends Controller_Template
 {
 	protected $auth;
 	protected $login_required = TRUE;
+	protected $role_required = FALSE;
 
 	public function before()
 	{
@@ -13,6 +14,9 @@ class Controller_Kadmium_Auth extends Controller_Template
 			$this->require_login();
 		}
 		$this->template->is_logged_in = $this->auth->logged_in();
+		if ($this->role_required) {
+			$this->require_role($this->role_required);
+		}
 		if ($this->template->is_logged_in) {
 			$this->template->username = $this->auth->get_user()->username;
 		}
@@ -21,6 +25,13 @@ class Controller_Kadmium_Auth extends Controller_Template
 	protected function require_login()
 	{
 		if($this->auth->logged_in() == 0) {
+			throw new Kadmium_Exception_NoPermission();
+		}
+	}
+
+	protected function require_role($role)
+	{
+		if (!$this->template->is_logged_in || !$this->auth->get_user()->has_role($role)) {
 			throw new Kadmium_Exception_NoPermission();
 		}
 	}
