055a9490d47f80fbb12b7c1c92f2763f01cb919c
Kelvin Luck <git@kelvinluck.com>
2010-12-02 11:08:17 +0000

    Added a link to enable you to easily log back in to the admin section after logging out...

--- classes/controller/kadmium/user.php
+++ classes/controller/kadmium/user.php
@@ -48,7 +48,10 @@ abstract class Controller_Kadmium_User extends Controller_Kadmium
 			'Logged out!'
 		);
 		$this->template->content = View::factory(
-			'kadmium/user/logged_out'
+			'kadmium/user/logged_out',
+			array(
+				'admin_url' => Kohana::config('kadmium')->base_path
+			)
 		);
 	}
 
--- views/kadmium/user/logged_out.php
+++ views/kadmium/user/logged_out.php
@@ -1,2 +1,7 @@
 <h1>Logged out</h1>
-<p>Thank you - you are now logged out.</p>
+<p>Thank you - you are now logged out. <?=
+	Html::anchor(
+		$admin_url,
+		'Log back in'
+	);
+	?>.</p>
