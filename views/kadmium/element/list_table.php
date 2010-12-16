0d64b2df43f1ea3bd632a790025b39fec3d6e1f7
Kelvin Luck <git@kelvinluck.com>
2010-12-16 17:58:57 +0000

    Pulled the part of list which renders the table out into it's own view for reuse...

---
+++ views/kadmium/element/list_table.php
@@ -0,0 +1,72 @@
+
+<table class="list-page">
+<?php
+	foreach ($items as $item):
+		if (!isset($fields)) {
+			$fields = $item->meta()->fields();
+?>
+	<thead>
+		<tr>
+<?php
+			foreach ($fields as $field_id => $field):
+				if (!$field->show_in_list):
+					continue;
+				endif;
+?>
+			<th><?= $field->label; ?></th>
+<?php
+			endforeach;
+			if ($show_edit):
+?>
+			<th>&nbsp;</th>
+<?php
+			endif;
+?>
+		</tr>
+	</thead>
+	<tbody>
+<?php
+		}
+?>
+<tr>
+<?php
+		foreach ($fields as $field_id => $field):
+			if (!$field->show_in_list):
+				continue;
+			endif;
+?>
+			<td><?= $field->display($item, $item->get($field_id)); ?></td>
+<?php
+		endforeach;
+		if ($show_edit):
+?>
+	<td>
+		<?php
+			if ($extra_button_view != ''){
+				echo View::factory(
+					$extra_button_view,
+					array(
+						'item' => $item,
+					)
+				);
+			}
+			echo Html::anchor(
+				Route::get('kadmium')
+					->uri(array(
+						'controller' => Request::instance()->controller,
+						'action' => 'edit',
+						'id' => $item->id(),
+					)),
+				'Edit'
+			);
+		?>
+	</td>
+<?php
+		endif;
+?>
+</tr>
+<?php
+	endforeach;
+?>
+	</tbody>
+</table>
--- views/kadmium/list.php
+++ views/kadmium/list.php
@@ -26,79 +26,15 @@ else:
 <p>
 	<?= $add_link; ?>
 </p>
-<table class="list-page">
 <?php
-	foreach ($items as $item):
-		if (!isset($fields)) {
-			$fields = $item->meta()->fields();
-?>
-	<thead>
-		<tr>
-<?php
-			foreach ($fields as $field_id => $field):
-				if (!$field->show_in_list):
-					continue;
-				endif;
-?>
-			<th><?= $field->label; ?></th>
-<?php
-			endforeach;
-			if ($show_edit):
-?>
-			<th>&nbsp;</th>
-<?php
-			endif;
-?>
-		</tr>
-	</thead>
-	<tbody>
-<?php
-		}
-?>
-<tr>
-<?php
-		foreach ($fields as $field_id => $field):
-			if (!$field->show_in_list):
-				continue;
-			endif;		
-?>
-			<td><?= $field->display($item, $item->get($field_id)); ?></td>
-<?php
-		endforeach;
-		if ($show_edit):
-?>
-	<td>
-		<?php
-			if ($extra_button_view != ''){
-				echo View::factory(
-					$extra_button_view,
-					array(
-						'item' => $item,
-					)
-				);
-			}
-			echo Html::anchor(
-				Route::get('kadmium')
-					->uri(array(
-						'controller' => Request::instance()->controller,
-						'action' => 'edit',
-						'id' => $item->id(),
-					)),
-				'Edit'
-			);
-		?>
-	</td>
-<?php
-		endif;
-?>
-</tr>
-<?php
-	endforeach;
-?>
-	</tbody>
-</table>
-
-<?php
+	echo View::factory(
+		'kadmium/element/list_table',
+		array(
+			'items' => $items,
+			'show_edit' => $show_edit,
+			'extra_button_view' => $extra_button_view,
+		)
+	);
 	echo $pagination;
 endif;
 ?>
