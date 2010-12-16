ef2118b57abcb4c9afd697e4607f6a866577283d
Kelvin Luck <git@kelvinluck.com>
2010-12-16 17:41:03 +0000

    Util method to get a copy of all of the non relational fields from a model with HTML characters escaped so they are clean and safe for output...

--- classes/kadmium/model/core.php
+++ classes/kadmium/model/core.php
@@ -11,4 +11,17 @@ abstract class Kadmium_Model_Core extends Jelly_Model_Core
 	const DELETE_ONLY_SPINSTER = 2;
 
 	public $delete_policy = Kadmium_Model_Core::DELETE_ONLY_SPINSTER;
+
+	// Get's the non-relational fields of this model cleaned for output...
+	public function get_xss_cleaned()
+	{
+		$cleaned = array();
+		foreach($this->meta()->fields() as $field_id=>$field) {
+			if ($field instanceof Jelly_Field_Relationship) {
+				continue;
+			}
+			$cleaned[$field_id] = Html::chars($this->get($field_id));
+		}
+		return (object)$cleaned;
+	}
 }
