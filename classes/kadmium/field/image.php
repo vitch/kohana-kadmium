b3c19a7804b4055b8b9dda95160f662cd1649bfe
Kelvin Luck <git@kelvinluck.com>
2011-03-20 18:50:38 +0000

    Allowing the web path to be set independantly of the file path so that you can upload images that are accessible outside of the filesystem structure of the admin site (e.g. on a subdomain)

--- classes/kadmium/field/image.php
+++ classes/kadmium/field/image.php
@@ -3,9 +3,17 @@
 abstract class Kadmium_Field_Image extends Jelly_Field_Image
 {
 
+	public $web_path = '';
+
 	public function display($model, $value)
 	{
 		$path = count($this->thumbnails) ? $this->thumbnails[0]['path'] : $this->path;
-		return $value == '' ? $value : Html::image(str_replace(DOCROOT, '', $path).$value);
+		return $value == '' ? $value : Html::image($this->get_web_path($path).$value);
 	}
+
+	public function get_web_path($path)
+	{
+		return $this->web_path ? $this->web_path : str_replace(DOCROOT, '', $path);
+	}
+
 }
--- views/jelly/field/image.php
+++ views/jelly/field/image.php
@@ -4,17 +4,18 @@
 	// If there is an uploaded image then display it.
 	// If there are thumbnails then display those instead.
 	if ($value != '' && !is_array($model->{$field->name})):
+
 		echo '<ul class="imagefiles">';
 		if (count($field->thumbnails)) {
 			for ($i = count($field->thumbnails); $i--; $i > -1) {
 				$thumbnail = $field->thumbnails[$i];
 				echo '<li>';
-				echo Html::image(str_replace(DOCROOT, '', $thumbnail['path']) . $model->{$field->name});
+				echo Html::image($field->get_web_path($thumbnail['path']) . $model->{$field->name});
 				echo '</li>';
 			}
 		} else {
 			echo '<li>';
-			echo Html::image(str_replace(DOCROOT, '', $field->path) . $model->{$field->name});
+			echo Html::image($field->get_web_path($field->path) . $model->{$field->name});
 			echo '</li>';
 		}
 		echo '</ul>';
