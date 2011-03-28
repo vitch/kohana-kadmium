6d3897d05f64772519e5bfbb4ad689ac79c522d7
Kelvin Luck <git@kelvinluck.com>
2011-03-28 15:39:45 +0100

    Allowing inputs of type file to have a webpath rather than assuming that they are uploaded under the DOCROOT...

--- classes/kadmium/field/file.php
+++ classes/kadmium/field/file.php
@@ -2,6 +2,7 @@
 
 abstract class Kadmium_Field_File extends Jelly_Field_File
 {
+	public $web_path = '';
 
 	/**
 	 * Adds the CSS class I want all of my textfields to have.
@@ -14,5 +15,10 @@ abstract class Kadmium_Field_File extends Jelly_Field_File
 	{
 		parent::initialize($model, $column);
 		array_push($this->css_class, 'inp-text');
+	}
+
+	public function get_web_path($path, $thumbnail_index = -1)
+	{
+		return $this->web_path ? $this->web_path : str_replace(DOCROOT, '', $path);
 	}
 }
---
+++ views/jelly/field/file.php
@@ -0,0 +1,15 @@
+<?php
+echo Form::file($name, $attributes + array('id' => 'field-'.$name));
+if ($value != '' && !is_array($model->{$field->name})):
+	echo '<label>Current file:</label>';
+	echo '<div style="clear: left">';
+	echo Html::anchor(
+		$field->get_web_path($field->path) . $model->{$field->name},
+		$model->{$field->name},
+		array(
+			'target' => '_blank'
+		)
+	);
+	echo '</div>';
+endif;
+?>
