289ae7ee2de73477d59d5625a5f41e49cbac08b3
Kelvin Luck <git@kelvinluck.com>
2011-07-11 15:20:56 +0100

    Added a pretty display option for Field_Options...

--- classes/kadmium/field/options.php
+++ classes/kadmium/field/options.php
@@ -7,4 +7,17 @@ class Kadmium_Field_Options extends Kadmium_Field_Text
 		$data['options'] = $this->options;
 		return parent::input($prefix, $data);
 	}
+
+	/**
+	 * Returns a particular value processed according
+	 * to the class's standards.
+	 *
+	 * @param   Jelly_Model  $model
+	 * @param   mixed        $value
+	 * @return  string
+	 **/
+	public function display($model, $value)
+	{
+		return $this->options[$value];
+	}
 }
