14c7b82ae8d261c738b609c339ab0a5074af6452
Kelvin Luck <git@kelvinluck.com>
2011-08-03 17:31:40 +0100

    List page pagination correctly with the new Pagination module...

--- views/kadmium/element/pagination.php
+++ views/kadmium/element/pagination.php
@@ -1,11 +1,11 @@
 <ul class="pagination">
 
-	<?php if ($previous_page !== FALSE): ?>
-		<li class="link-back"><a href="<?php echo $page->url($previous_page) ?>">prev</a></li>
+	<?php if ($page->previous_page() !== FALSE): ?>
+		<li class="link-back"><a href="<?php echo $page->url($page->previous_page()) ?>">prev</a></li>
 	<?php endif ?>
 
-	<?php for ($i = 1; $i <= $total_pages; $i++): ?>
-		<li<?php if ($i == $current_page):
+	<?php for ($i = 1; $i <= $page->total_pages(); $i++): ?>
+		<li<?php if ($i == $page->current_page()):
 			echo ' class="selected"';
 		endif;
 		?>>
@@ -13,8 +13,8 @@
 		</li>
 	<?php endfor ?>
 
-	<?php if ($next_page !== FALSE): ?>
-		<li class="link"><a href="<?php echo $page->url($next_page) ?>">next</a></li>
+	<?php if ($page->next_page() !== FALSE): ?>
+		<li class="link"><a href="<?php echo $page->url($page->next_page()) ?>">next</a></li>
 	<?php endif ?>
 
 
