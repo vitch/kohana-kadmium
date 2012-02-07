f51b8e380c93e083b50d29bfc84815bdddba22d3
Kelvin Luck <git@kelvinluck.com>
2012-02-07 17:51:57 +0000

    Fix for Field_HasMany display...

--- classes/kadmium/core/field/hasmany.php
+++ classes/kadmium/core/field/hasmany.php
@@ -3,4 +3,18 @@
 abstract class Kadmium_Core_Field_HasMany extends Jelly_Core_Field_HasMany
 {
 	var $show_in_list = FALSE;
+
+	public function input($prefix = 'jelly/field', $data = array())
+	{
+		$data['ids'] = array();
+		foreach ($data['value'] as $model)
+		{
+			$data['ids'][] = $model->id();
+		}
+		// Could use
+		// $data['ids'] = $data['value']->as_array(null, 'id');
+		// but how to generalise it so that "id" isn't hard coded?
+
+		return parent::input($prefix, $data);
+	}
 }
