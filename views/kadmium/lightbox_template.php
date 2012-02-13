a1e97f7abc8dc48c9db7464b5ed8357891bafa5e
Kelvin Luck <git@kelvinluck.com>
2012-02-13 10:40:29 +0000

    Adding jQuery back in to the lightbox template...

--- views/kadmium/lightbox_template.php
+++ views/kadmium/lightbox_template.php
@@ -7,6 +7,8 @@
 
 		<?php foreach ($styles as $style => $media) echo HTML::style($style, array('media' => $media), TRUE), "\n" ?>
 
+		<script src="//ajax.googleapis.com/ajax/libs/jquery/1.7.1/jquery.min.js"></script>
+		<script>window.jQuery || document.write('<script src="/kadmium/media/js/jquery-1.7.1.min.js"><\/script>');</script>
 		<?php foreach ($scripts as $script) echo HTML::script($script, NULL, TRUE), "\n" ?>
 	</head>
 	<body>
