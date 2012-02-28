68f67834e9978236352bfd5e55241492b048ff0e
Kelvin Luck <git@kelvinluck.com>
2012-02-28 19:51:57 +0000

    Adding a class to the multiple select which is the default view for a HasMany as a temporary fix.

--- views/jelly/field/manytomany.php
+++ views/jelly/field/manytomany.php
@@ -1,4 +1,4 @@
-<select name="<?php echo $name ?>[]" <?php echo HTML::attributes($attributes + array('id' => 'field-'.$name)) ?> multiple="multiple">
+<select name="<?php echo $name ?>[]" <?php echo HTML::attributes($attributes + array('id' => 'field-'.$name, 'class' => 'span7')) ?> multiple="multiple">
 	<?php foreach(Jelly::query($foreign['model'])->select() as $related): ?>
 		<?php if (in_array($related->id(), $ids)): ?>
 			<option value="<?php echo $related->id() ?>" selected='selected'><?php echo $related->name()?></option>
