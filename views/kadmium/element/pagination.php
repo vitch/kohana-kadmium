4d1de2cfe12377755294ff07ccb0bc92dee1723b
Kelvin Luck <git@kelvinluck.com>
2013-12-13 20:07:26 +1300

    Much nicer implementation of floating pagination.
    Simpler code and conforms to Yahoo's design pattern for pagination
    too:
    
    http://developer.yahoo.com/ypatterns/navigation/pagination/search.html

--- views/kadmium/element/pagination.php
+++ views/kadmium/element/pagination.php
@@ -1,115 +1,60 @@
-<div class="alert">
-	<h2><?= $page->total_items(); ?> results <small>viewing <?= $page->current_first_item(); ?>-<?= $page->current_last_item(); ?></small></h2>
-</div>
-
-<div class="pagination pagination-centered">
+<div class="pagination">
 
 	<ul>
 
 		<?php
 
-			if ($page->previous_page() === FALSE){
-				echo '<li class="prev disabled">';
-				echo Html::anchor(
-					'#',
-					'&larr;'
-				);
-				echo '</li>';
-			} else {
-				echo '<li class="prev">';
-				echo Html::anchor(
-					$page->url($page->previous_page()),
-					'&larr;'
-				);
-				echo '</li>';
-			}
-
-			// Number of page links in the begin and end of whole range
-			$count_out = ( ! empty($config['count_out'])) ? (int) $config['count_out'] : 4;
-			// Number of page links on each side of current page
-			$count_in = ( ! empty($config['count_in'])) ? (int) $config['count_in'] : 5;
-
-			// Beginning group of pages: $n1...$n2
-			$n1 = 1;
-			$n2 = min($count_out, $page->total_pages());
-
-			// Ending group of pages: $n7...$n8
-			$n7 = max(1, $page->total_pages() - $count_out + 1);
-			$n8 = $page->total_pages();
-
-			// Middle group of pages: $n4...$n5
-			$n4 = max($n2 + 1, $page->current_page() - $count_in);
-			$n5 = min($n7 - 1, $page->current_page() + $count_in);
-			$use_middle = ($n5 >= $n4);
-
-			// Point $n3 between $n2 and $n4
-			$n3 = (int) (($n2 + $n4) / 2);
-			$use_n3 = ($use_middle && (($n4 - $n2) > 1));
-
-			// Point $n6 between $n5 and $n7
-			$n6 = (int) (($n5 + $n7) / 2);
-			$use_n6 = ($use_middle && (($n7 - $n5) > 1));
-
-			// Links to display as array(page => content)
 			$links = array();
 
-			// Generate links data in accordance with calculated numbers
-			for ($i = $n1; $i <= $n2; $i++)
-			{
-				$links[$i] = true;
-			}
-			if ($use_n3)
-			{
-				$links[$n3] = false;
-			}
-			for ($i = $n4; $i <= $n5; $i++)
-			{
-				$links[$i] = true;
-			}
-			if ($use_n6)
-			{
-				$links[$n6] = false;
-			}
-			for ($i = $n7; $i <= $n8; $i++)
-			{
-				$links[$i] = true;
-			}
+			// Previous link
+			$links[] = array(
+				'class' => 'prev' . ($page->previous_page() === FALSE ? ' disabled' : ''),
+				'link' => $page->previous_page() === FALSE ? '#' : $page->url($page->previous_page()),
+				'content' => '&larr;',
+			);
 
-			$pages = range(1, $page->total_pages());
+			// Calculate which page links to display
+			$max_pages_to_display = (!empty($config['max_pages_to_display'])) ? (int) $config['max_pages_to_display'] : 18;
 
-			foreach ($links as $i => $is_link) {
-				$css_class = '';
-				if (!$is_link) {
-					$css_class = 'disabled';
-				} else if($i === $page->current_page()) {
-					$css_class = 'active';
+			$num_pages = $page->total_pages();
+			$first_page = 1;
+			$current_page = $page->current_page();
+			$last_page = $num_pages;
+			if ($num_pages > $max_pages_to_display) {
+				$first_page = $current_page - floor($max_pages_to_display / 2);
+				$last_page = $current_page + floor($max_pages_to_display / 2);
+				while($first_page < 1) {
+					$first_page++;
+					$last_page ++;
 				}
-				echo '<li class="' . $css_class . '">';
-				if ($is_link) {
-					echo Html::anchor(
-						$page->url($i),
-						($i < 10 ? '&nbsp;' : '') . $i
-					);
-				} else {
-					echo Html::anchor('#', '...');
+				while($last_page > $num_pages) {
+					$first_page--;
+					$last_page--;
 				}
-				
-				echo '</li>';
 			}
 
-
-			if ($page->next_page() === FALSE){
-				echo '<li class="next disabled">';
-				echo Html::anchor(
-					'#',
-					'&rarr;'
+			// Create link items for each page
+			foreach(range($first_page, $last_page) as $i) {
+				$links[] = array(
+					'class' => $i == $current_page ? 'active' : '',
+					'link' => $page->url($i),
+					'content' => ($i < 10 ? '&nbsp;' : '') . $i
 				);
-				echo '</li>';
-			} else {
-				echo '<li class="next">';
+			}
+
+			// Next link
+			$links[] = array(
+				'class' => 'next' . ($page->next_page() === FALSE ? ' disabled' : ''),
+				'link' => $page->next_page() === FALSE ? '#' : $page->url($page->next_page()),
+				'content' => '&rarr;',
+			);
+
+			// Render pages
+			foreach ($links as $link) {
+				echo '<li class="' . $link['class'] . '">';
 				echo Html::anchor(
-					$page->url($page->next_page()),
-					'&rarr;'
+					$link['link'],
+					$link['content']
 				);
 				echo '</li>';
 			}
