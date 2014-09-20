24aebc6aad5e1ceda3d7da156798f79ea7d98792
Kelvin Luck <git@kelvinluck.com>
2014-09-20 07:51:02 +0200

    Protocol relative link to jQuery...

--- views/kadmium/template.php
+++ views/kadmium/template.php
@@ -7,7 +7,7 @@
 
 		<?php foreach ($styles as $style => $media) echo HTML::style($style, array('media' => $media), TRUE), "\n\t\t" ?>
 
-		<?= Html::script('http://ajax.googleapis.com/ajax/libs/jquery/1.4.4/jquery.min.js'); ?>
+		<?= Html::script('//ajax.googleapis.com/ajax/libs/jquery/1.4.4/jquery.min.js'); ?>
 		<script>window.jQuery || document.write('<script src="<?= Url::site(Route::get('kadmium/media')->uri(array('file' => 'js/jquery-1.4.4.min.js'))); ?>"><\/script>')</script>
 		<?php foreach ($scripts as $script) echo HTML::script($script, NULL, TRUE), "\n\t\t" ?>
 
