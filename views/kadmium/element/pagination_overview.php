ad3834ed3d1cbd7a0fbb58cc45da052171cf0c26
Kelvin Luck <git@kelvinluck.com>
2015-03-14 10:45:38 +0000

    Cleaning up space around text in pagination overview...

--- views/kadmium/element/pagination_overview.php
+++ views/kadmium/element/pagination_overview.php
@@ -1,11 +1,10 @@
 <div class="alert">
 	<h4>
-		Viewing <?= number_format($page->current_first_item()); ?>-<?= number_format($page->current_last_item()); ?>
-		<small>of <?= number_format($page->total_items()); ?> results<?php
+		Viewing <?= number_format($page->current_first_item()); ?>-<?= number_format($page->current_last_item()); ?> <small>of <?= number_format($page->total_items()); ?> results<?php
       if ($display_search) {
         $q = Arr::get($_GET, 'q');
         if ($q) {
-          echo ' matching "<atrong>' . $q . '</strong>"';
+          echo ' matching "<strong>' . $q . '</strong>"';
         }
       }
     ?></small>
